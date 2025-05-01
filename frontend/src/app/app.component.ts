4b11f93ac779acc1353d88e543b7746d292a62f5
Colin Akard <cakard@onecause.com>
2025-05-01 14:13:18 -0400

    better nav menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,28 +1,21 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
+import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { MatIconButton } from '@angular/material/button';
-import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
-import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIconAnchor } from '@angular/material/button';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
-import { MatListItem, MatNavList } from '@angular/material/list';
-import { filter } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatNavList } from '@angular/material/list';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
   imports: [
-    MatToolbar,
     MatIcon,
-    MatIconButton,
-    MatSidenavContent,
-    MatSidenavContainer,
-    MatSidenav,
     RouterLink,
     RouterOutlet,
     MatNavList,
-    MatListItem,
-    RouterLinkActive
+    RouterLinkActive,
+    MatIconAnchor,
+    MatTooltip,
   ],
   providers: [
     {
@@ -34,15 +27,4 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  @ViewChild('sidenav') menuSidenav?: MatSidenav;
-
-  constructor(
-    private router: Router,
-  ) {
-    this.router.events.pipe(
-      filter((ev): ev is NavigationEnd => ev instanceof NavigationEnd),
-      takeUntilDestroyed(),
-    ).subscribe(() => this.menuSidenav?.close());
-  }
 }
